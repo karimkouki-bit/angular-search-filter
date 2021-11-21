bc39b387e37bbc56cebbf8ccf726637842b6bf93
karim <karim54@gmail.com>
2021-11-21 15:09:42 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  term:any;
+  list = [
+    {id: '1', 'name': 'karim', 'email': 'karim456@gmail.com', 'city': 'sousse'},
+    { id: '2','name': 'amin', 'email': 'amin456@gmail.com', 'city': 'hamamam sousse'},
+    { id: '3','name': 'rayen', 'email': 'rayen456@gmail.com', 'city': 'nefza'},
+    { id: '4','name': 'samir', 'email': 'samir456@gmail.com', 'city': 'tunis'},
+    { id: '5','name': 'shiraz', 'email': 'shiraz456@gmail.com', 'city': 'monastir'},
+    { id: '6','name': 'farah', 'email': 'karim456@gmail.com', 'city': 'mahdia'},
+  ]
+
+  isOpen:boolean = false
+
   title = 'search-filter-angular';
+
+  togglenavbar(){
+    this.isOpen = !this.isOpen
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
